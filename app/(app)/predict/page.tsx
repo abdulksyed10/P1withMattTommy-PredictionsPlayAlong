4ccecfaefd5fe17d6e8f5dce8c79f4e98cb7aa94
Abdul Kalam Syed <abdulksyed10@gmail.com>
2026-01-22 18:19:06 -0800

    Intial page routing setup completed

---
+++ app/(app)/predict/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold">P1 Predictions</h1>
+    </main>
+  );
+}
