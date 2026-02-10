e3dfd1ca2e0c9d5c10ac390ff41c18dadcee7d14
Abdul Kalam Syed <abdulksyed10@gmail.com>
2026-02-09 23:12:52 -0800

    working on landing page. Currently redirecting to login page.

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,7 +1,10 @@
+import { redirect } from "next/dist/client/components/navigation";
+
 export default function Home() {
-  return (
-    <main className="min-h-screen flex items-center justify-center bg-black text-white">
-      <h1 className="text-4xl font-bold">P1 Predictions</h1>
-    </main>
-  );
+  // return (
+  //   <main className="min-h-screen flex items-center justify-center bg-black text-white">
+  //     <h1 className="text-4xl font-bold">P1 Predictions</h1>
+  //   </main>
+  // );
+  redirect("/login");
 }
